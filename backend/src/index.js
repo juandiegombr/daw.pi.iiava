4eab336e4518112eff95b02c110e19b1cf02c502
juandiegombr <juandiegomartinblas@gmail.com>
2025-11-03 20:30:06 +0100

    Add logger

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import pinoHttp from "pino-http";
+import { logger } from "./logger.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(pinoHttp({ logger }));
+
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
