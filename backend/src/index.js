00320e833ed9ece87c59dbe728c84772a63d4b76
juandiegombr <juandiegomartinblas@gmail.com>
2025-11-03 20:14:28 +0100

    Connect to database at start

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,23 +3,11 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
-const FarmerSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-}, { collection: "farmer" });
-
-const Farmer = mongoose.model("Farmer", FarmerSchema);
-
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-app.get("/api/fields", async (req, res) => {
-  const farmers = await Farmer.find();
-  res.json({ data: { farmers } });
-});
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
@@ -28,5 +16,17 @@ mongoose
   .connect(MONGO_URI)
   .then(() => console.log("✅ MongoDB conectado"))
   .catch((err) => console.error("❌ Error conectando a Mongo:", err));
+
+const FarmerSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+}, { collection: "farmer" });
+
+const Farmer = mongoose.model("Farmer", FarmerSchema);
+
+app.get("/api/fields", async (req, res) => {
+  const farmers = await Farmer.find();
+  res.json({ data: { farmers } });
+});
 
 app.listen(PORT, () => console.log(`🚀 Backend corriendo en puerto ${PORT}`));
