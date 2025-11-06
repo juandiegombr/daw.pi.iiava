bdd5fe73abdd16a616039ebb073e8880c370213d
juandiegombr <juandiegomartinblas@gmail.com>
2025-11-06 21:43:47 +0100

    Fix Tailwind CSS usage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-/* eslint-disable */ console.log('ssss', )
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
